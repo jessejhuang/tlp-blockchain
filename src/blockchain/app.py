97d6afcd47be1b9271e04fa119cd19e9668c7a50
Jesse Huang <jessehuang@wustl.edu>
2018-06-27 20:46:25 -0500

    test flask-flask communication

--- src/blockchain/app.py
+++ src/blockchain/app.py
@@ -3,7 +3,6 @@ from node import Node
 
 app = Flask(__name__)
 
-@app.before_first_request
 def connect_to_network():
     """ Get the origin node's peers"""
     origin_url = 'http://174.138.127.161:5000'
@@ -12,6 +11,14 @@ def connect_to_network():
         node.request_peers(origin_url)
         setNode(node)
 
+
+@app.route('/peers', methods=['POST'])
+def send_peers():
+    node = getNode()
+    if len(node.peers) > 0:
+        return node.peers
+    else:
+        return "No other nodes are connected."
 
 def getNode():
     node = getattr(g, '_node', None)
@@ -23,4 +30,5 @@ def setNode(node):
     g._node = node 
 
 if __name__ == "__main__":
+    connect_to_network()
     app.run(host='0.0.0.0')
