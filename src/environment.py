9d9c31d4b497856988475b2249c4b2811ae4c5cc
Jesse Huang <jessehuang@wustl.edu>
2018-08-05 15:35:08 -0500

    refactor environment
    move origin_nodes to .flaskenv

--- src/environment.py
+++ src/environment.py
@@ -8,7 +8,7 @@ from dotenv import dotenv_values
 FLASKENV_VARIABLES = dotenv_values(stream='.flaskenv')
 PORT = FLASKENV_VARIABLES['FLASK_RUN_PORT']
 HOST = FLASKENV_VARIABLES['FLASK_RUN_HOST']
-ORIGIN_NODES = ('67.205.135.0',)
+ORIGIN_NODES = FLASKENV_VARIABLES['ORIGIN_NODES'].split(',')
 ORIGIN_IP = random.choice(ORIGIN_NODES)
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
